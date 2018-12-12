026aeec8bdcc77c51576049f2536e03031158e75
larmax <leon.miller@hotmail.fi>
2018-12-12 13:19:19 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Route } from 'react-router-dom';
+
 
 
 class App extends Component {
@@ -19,8 +21,8 @@ class App extends Component {
       <div className="App">
 <Layout>
 
-
 <Route path="/checkout" component={Checkout} />
+<Route path="/orders" component={Orders} />
 <Route path="/" exact component={BurgerBuilder} />
 
 </Layout>
